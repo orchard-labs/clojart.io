4917ea911df2962a7ef53acb81bc15be301298ac
Mandarin Drummond <mandarind@gmail.com>
2014-10-02 16:32:33 -0400

    Added jade template and req'd dependencies, and necessary code in handler.clj

--- src/clojart/handler.clj
+++ src/clojart/handler.clj
@@ -1,10 +1,18 @@
 (ns clojart.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clj-jade.core :as jade]))
+
+(jade/configure {:template-dir "resources/views/"
+                 :pretty-print true
+                 :cache? true})
+
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (jade/render "index.jade" {"pageName" "clojart.io"
+                           "submissions" [{"author" "MandarinFL" "title" "io.clojart.foo"}
+                                          {"author" "j0ni" "title" "io.clojart.bar"}]}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
