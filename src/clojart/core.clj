73d4ae19289b5cf6e7e043c4bca515354bfc5105
J Irving <j@lollyshouse.ca>
2014-10-02 01:29:50 -0400

    Add a basic REPL

--- src/clojart/core.clj
+++ src/clojart/core.clj
@@ -2,8 +2,22 @@
   (:gen-class)
   (:require [org.httpkit.server :as http-kit]
             [clojart.handler :as app]
-            [compojure.handler :as h]))
+            [compojure.handler :as h]
+            [clojure.tools.nrepl.server :as nrepl]))
+
+;;
+;; REPL management and convenience stuff
+;;
+
+(defonce repl (atom nil))
+
+(defn start-repl [port]
+  (nrepl/start-server :port port))
+
+(defn stop-repl [repl]
+  (nrepl/stop-server repl))
 
 (defn -main
   [& args]
+  (reset! repl (start-repl 4005))
   (http-kit/run-server (h/site #'app/app) {:port 8080}))
