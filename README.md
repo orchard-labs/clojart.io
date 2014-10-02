2998283f0c4d7ee747d0871d8fe0c0b33851c5ec
J Irving <j@lollyshouse.ca>
2014-10-02 01:10:55 -0400

    Hello world compojure and http-kit app.

---
+++ README.md
@@ -0,0 +1,20 @@
+# clojart
+
+An application for making beautiful visualizations of code. Totally
+Fucking Orchard.
+
+## Prerequisites
+
+You will need [Leiningen][1] 1.7.0 or above installed.
+
+[1]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2014 Orchard Labs
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clojart "0.1.0-SNAPSHOT"
+  :description "Visualize code beautifully"
+  :url "http://github.com/orchard-labs/clojart.io"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [http-kit "2.1.16"]]
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler clojart.handler/app}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}
+             :uberjar {:aot :all}}
+  :main clojart.core)
