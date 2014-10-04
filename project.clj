1898f22a5256e2c0581551e26c0f70aefbb56b9c
MandarinConLaBarba <mandarind@gmail.com>
2014-10-03 20:37:48 -0400

    Added initial css..

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
                  [http-kit "2.1.16"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [garden "1.2.1"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler clojart.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
---
+++ src/clojart/styles/site.clj
@@ -0,0 +1,8 @@
+(ns clojart.styles.site
+  (:require [garden.core :refer [css]]))
+
+(css [:body {
+             :font-family "Helvetica"
+             :font-size "16px"}])
+
+
