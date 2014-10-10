b0be87522e6653a2cf759f90b2f3dc18fd62dcd3
Mandarin Drummond <mandarind@gmail.com>
2014-10-10 13:15:25 -0400

    Added Om deps, and restructured project to support cljs

--- project.clj
+++ project.clj
@@ -12,6 +12,10 @@
                  [compojure "1.2.0"]
                  [http-kit "2.1.16"]
 
+                 ;; Client stack
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [om "0.7.3"]
+
                  ;; HTML templates
                  [hiccup "1.0.5"]
 
@@ -22,17 +26,28 @@
                  [org.clojure/tools.nrepl "0.2.6"]
                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
 
+  :source-paths ["src/clj" "src/cljs"]
+
   :plugins [[lein-ring "0.8.12"]
             [lein-garden "0.2.1"]
+            [lein-cljsbuild "1.0.3"]
             [environ/environ.lein "0.2.1"]]
 
   :hooks [environ.leiningen.hooks]
 
   :garden {:builds [{:id "site"
-                     :source-paths ["src/"]
+                     :source-paths ["src/clj"]
                      :stylesheet clojart.styles.site/site
                      :compiler {:output-to "resources/public/site.css"
                                 :pretty-print? false}}]}
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                          :output-to "resources/public/js/main.js"
+                          :output-dir "resources/public/js/out"
+                          :optimizations :none
+                          :source-map true}}]}
 
   :ring {:handler clojart.handler/app}
 
